3d70d7f0064cda28d6bf14e3c09456e6c862402e
CITRUSR <savelypetrov2007@mail.ru>
2025-02-10 23:05:22 +0300

    add initial migration

---
+++ server/Domain/Entities/User.cs
@@ -0,0 +1,8 @@
+namespace server.Domain.Entities;
+
+public class User
+{
+    public Guid Id { get; set; }
+    public string Nickname { get; set; }
+    public string Password { get; set; }
+}
---
+++ server/Infrastructure/Sql/Tables/UserStatTable.cs
@@ -0,0 +1,9 @@
+namespace server.Infrastructure.Sql.Tables;
+
+public class UserStatTable
+{
+    public static readonly string TableName = "user_stats";
+    public static readonly string Wins = "wins";
+    public static readonly string Losses = "losses";
+    public static readonly string GamesCount = "games_count";
+}

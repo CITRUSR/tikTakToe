299a5cf2bde4d4fa2ccd5a26a4acef39937e2407
CITRUSR <savelypetrov2007@mail.ru>
2025-02-10 21:26:33 +0300

    add docker with database support

--- server/Extensions/StartupExtension.cs
+++ server/Extensions/StartupExtension.cs
@@ -1,10 +1,18 @@
+using server.Application;
+using server.Infrastructure;
+
 namespace server.Extensions;
 
 public static class StartupExtension
 {
-    public static void ConfigureServices(this IServiceCollection services)
+    public static void ConfigureServices(
+        this IServiceCollection services,
+        IConfiguration configuration
+    )
     {
         services.AddOpenApi();
+        services.AddInfrastructure(configuration);
+        services.AddApplication();
     }
 
     public static void ConfigureApp(this WebApplication app)

7f5d82126354fd0ca9ca301b92cb188dfd22e1e9
CITRUSR <savelypetrov2007@mail.ru>
2025-02-18 23:33:08 +0300

    sq before add trigger after insert user

--- server/Infrastructure/Migrations/002_AddInsertUserTrigger.sql
+++ server/Infrastructure/Migrations/002_AddInsertUserTrigger.sql
@@ -1,14 +1,13 @@
 CREATE OR REPLACE FUNCTION add_user_stats()
 RETURNS TRIGGER
-LANGUAGE PLPGSQL
 AS
 $$
 BEGIN 
     INSERT INTO user_stats (user_id)
     VALUES (NEW.id);
-    RETURN NEW;
+    RETURN NULL;
 END;
-$$
+$$ LANGUAGE plpgsql;
 
 CREATE TRIGGER add_stats AFTER INSERT ON users
-EXECUTE FUNCTION add_user_stats();
+FOR EACH ROW EXECUTE FUNCTION add_user_stats();

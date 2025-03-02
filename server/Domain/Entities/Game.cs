c5e0f3465b8662e02bd7fc0cfa0f6e6cee09997f
CITRUSR <savelypetrov2007@mail.ru>
2025-03-03 01:48:52 +0300

    add game finisher

--- server/Domain/Entities/Game.cs
+++ server/Domain/Entities/Game.cs
@@ -1,10 +1,12 @@
+using server.Application.Services.User.Dtos.Responses;
+
 namespace server.Domain.Entities;
 
 public class Game
 {
     public Guid Id { get; set; }
-    public List<User> Players { get; set; }
-    public User Winner { get; set; }
+    public List<UserViewModel> Players { get; set; }
+    public UserViewModel? Winner { get; set; }
     public TimeSpan StartTime { get; set; }
     public TimeSpan EndTime { get; set; }
 }
